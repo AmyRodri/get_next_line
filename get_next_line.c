ff3ba50a0cf5fae48491ab703c32659148478e84
amy rodrigues <rmmgustavo@gmail.com>
2025-07-29 15:46:44 -0300

    add arquivos

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 11:17:07 by amyrodri          #+#    #+#             */
+/*   Updated: 2025/07/28 11:35:26 by amyrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+// char	*get_next_line(int fd)
+// {
+// 	char	buffer[];
+// 	int		bytes;
+
+// 	if (fd == -1)
+// 		return (NULL);
+// 	bytes = read(fd, buffer, sizeof(buffer));
+// 	while (bytes != '\n')
+// 	{
+// 	}
+// }
