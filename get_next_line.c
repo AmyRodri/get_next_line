f39f2d3031f0031faccd523a656f37a4f0aadc84
amy rodrigues <rmmgustavo@gmail.com>
2025-08-04 16:08:23 -0300

    copy all

---
+++ get_next_line.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 11:17:07 by amyrodri          #+#    #+#             */
+/*   Updated: 2025/08/04 11:42:00 by amyrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	new_line(const char *s)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == '\n')
+			return (1);
+		s++;
+	}
+	return (0);
+}
+
+char	*extract_line_and_stash(char *line, char **stash)
+{
+	char	*new_line;
+	int		i;
+
+	if (!line || line[0] == '\0')
+		return (NULL);
+	i = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (line[i] == '\n')
+		i++;
+	new_line = ft_substr(line, 0, i);
+	if (!new_line)
+	{
+		free(line);
+		return (NULL);
+	}
+	if (line[i] != '\0')
+		*stash = ft_strdup(line + i);
+	else
+		*stash = NULL;
+	free(line);
+	return (new_line);
+}
+
+void	*free_null(char *buffer, char *line)
+{
+	free(buffer);
+	free(line);
+	return (NULL);
+}
+
+char	*read_line(char *line, int fd)
+{
+	int			bytes;
+	char		*buffer;
+	char		*tmp;
+
+	if (!line)
+		line = ft_strdup("");
+	buffer = malloc (BUFFER_SIZE + 1);
+	if (!buffer || !line)
+		return (free_null(buffer, line));
+	bytes = 1;
+	while (!new_line(line) && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes == -1)
+			return (free_null(buffer, line));
+		buffer[bytes] = '\0';
+		tmp = ft_strjoin(line, buffer);
+		free(line);
+		if (!tmp)
+			return (free_null(buffer, line));
+		line = tmp;
+	}
+	free(buffer);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (stash)
+	{
+		line = ft_strdup(stash);
+		free(stash);
+		stash = NULL;
+	}
+	else
+		line = NULL;
+	line = read_line(line, fd);
+	if (!line)
+		return (free(stash), stash = NULL);
+	if (new_line(line))
+		line = extract_line_and_stash(line, &stash);
+	else
+		stash = NULL;
+	if (!line || *line == '\0')
+		return (free(line), free(stash), stash = NULL);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 11:17:12 by amyrodri          #+#    #+#             */
+/*   Updated: 2025/08/04 11:45:44 by amyrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <unistd.h>
+
+int		new_line(const char *s);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*free_null(char *buffer, char *line);
+size_t	ft_strlen(const char *s);
+
+#endif
